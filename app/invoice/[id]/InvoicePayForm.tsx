99d1c718ad9ff66ecb0cd6a1bb9d5ece1c4aedbf
dropverseagency <3.06377369e+08+dropverseagency@users.noreply.github.com>
2026-08-19 05:49:50 +0000

    Fix wallet icon path syntax on invoice pay form

--- app/invoice/[id]/InvoicePayForm.tsx
+++ app/invoice/[id]/InvoicePayForm.tsx
@@ -145,7 +145,7 @@ function WalletIcon() {
     <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="#10221f" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
       <path d="M21 12V7a2 2 0 0 0-2-2H5a2 2 0 0 1 0-4h14v4" />
       <path d="M3 5v14a2 2 0 0 0 2 2h16v-5" />
-      <path d="M18 12a2 0 0 0 0 4h4v-4h-4z" />
+      <path d="M18 12a2 2 0 0 0 0 4h4v-4h-4z" />
     </svg>
   )
 }
